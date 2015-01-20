2ef8092741c10b91a25acd7ec0fa6ad8d94f6339
Aaron Storck <aaronstorck@gmail.com>
2015-01-20 14:54:31 -0500

    Initial work for sanitizaiton library.

---
+++ blueprint.js
@@ -0,0 +1,14 @@
+
+/**
+ * Blueprint for the Sanitize class.
+ *
+ */
+
+
+module.exports = {
+
+  toLower: {
+    description: "Will transform the '{{attribute}}' attribute to lowercase."
+  }
+
+};
